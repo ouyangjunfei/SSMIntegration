27d828a2534bfa73dd61651a14cf912c2a16d3df
ouyangjunfei <1342597034@qq.com>
2021-05-26 00:23:41 +0800

    BookController请求方式修改与添加只获取数据的Mapping

--- src/main/java/org/example/controller/BookController.java
+++ src/main/java/org/example/controller/BookController.java
@@ -6,8 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,48 +22,56 @@ public class BookController {
     }
 
 
-    //查询全部的书籍，并且返回到一个书籍展示页面
-    @RequestMapping(value = "/all")
-    public String queryAllBooks(Model model) {
+    // 查询全部的书籍，并且返回到一个书籍展示页面
+    @GetMapping(value = "/all")
+    public String queryAllBooksPage(Model model) {
         List<Book> bookList = bookService.queryAllBooks();
         model.addAttribute("list", bookList);
         return "book";
     }
 
+    // 查询全部书籍的数据
+    @PostMapping("/all")
+    @ResponseBody
+    public List<Book> queryAllBooksData() {
+        List<Book> bookList = bookService.queryAllBooks();
+        return bookList;
+    }
+
     //跳转到增加书籍页面
-    @RequestMapping("/addPage")
+    @GetMapping("/addPage")
     public String addBookPage() {
         return "addBook";
     }
 
     //添加一本书的具体内容
-    @RequestMapping("/add")
+    @GetMapping("/add")
     public String addBook(Book book) {
         bookService.addBook(book);
         return "redirect:/book/all";
     }
 
     //跳转到修改页面
-    @RequestMapping("/updatePage/{id}")
+    @GetMapping("/updatePage/{id}")
     public String updateBookPage(@PathVariable int id, Model model) {
         Book book = bookService.queryBookById(id);
         model.addAttribute("book", book);
         return "updateBook";
     }
 
-    @RequestMapping("/update")
+    @GetMapping("/update")
     public String updateBook(Book book) {
         bookService.updateBook(book);
         return "redirect:/book/all";
     }
 
-    @RequestMapping("/delete/{id}")
+    @GetMapping("/delete/{id}")
     public String deleteBook(@PathVariable int id) {
         bookService.deleteBookById(id);
         return "redirect:/book/all";
     }
 
-    @RequestMapping("/query")
+    @GetMapping("/query")
     public String queryBook(String bookName, Model model) {
         List<Book> bookList = bookService.queryBookByName(bookName);
         model.addAttribute("list", bookList);
