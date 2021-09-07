f839f1889abfa12558cc7ae1046bf903efe41c60
ouyangjunfei <1342597034@qq.com>
2021-09-07 23:40:56 +0800

    移动web文件夹的目录，修改换行符

--- sql/books.sql
+++ sql/books.sql
@@ -21,23 +21,33 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for books
 -- ----------------------------
 DROP TABLE IF EXISTS `books`;
-CREATE TABLE `books`  (
-  `book_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '书id',
-  `book_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '书名',
-  `book_count` int(11) NOT NULL COMMENT '数量',
-  `detail` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '描述',
-  `book_date` date NULL DEFAULT NULL COMMENT '出版时间',
-  PRIMARY KEY (`book_id`) USING BTREE,
-  INDEX `bookID`(`book_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `books`
+(
+    `book_id`    int(11)                                                 NOT NULL AUTO_INCREMENT COMMENT '书id',
+    `book_name`  varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '书名',
+    `book_count` int(11)                                                 NOT NULL COMMENT '数量',
+    `detail`     varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '描述',
+    `book_date`  date                                                    NULL DEFAULT NULL COMMENT '出版时间',
+    PRIMARY KEY (`book_id`) USING BTREE,
+    INDEX `bookID` (`book_id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 7
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of books
 -- ----------------------------
-INSERT INTO `books` VALUES (1, 'Java', 0, '从入门到放弃', '2021-04-01');
-INSERT INTO `books` VALUES (2, 'MySQL', 10, '从删库到跑路', NULL);
-INSERT INTO `books` VALUES (3, 'Linux', 5, '从进门到进牢', NULL);
-INSERT INTO `books` VALUES (5, '数据库原理', 8, '从删库到跑路v0.0.3', NULL);
-INSERT INTO `books` VALUES (6, 'Linux实战', 100, '从入门到入狱v0.0.2', NULL);
+INSERT INTO `books`
+VALUES (1, 'Java', 0, '从入门到放弃', '2021-04-01');
+INSERT INTO `books`
+VALUES (2, 'MySQL', 10, '从删库到跑路', NULL);
+INSERT INTO `books`
+VALUES (3, 'Linux', 5, '从进门到进牢', NULL);
+INSERT INTO `books`
+VALUES (5, '数据库原理', 8, '从删库到跑路v0.0.3', NULL);
+INSERT INTO `books`
+VALUES (6, 'Linux实战', 100, '从入门到入狱v0.0.2', NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
